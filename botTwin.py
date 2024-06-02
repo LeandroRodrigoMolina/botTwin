ab091012b44a449576b70deaa97d51a4baf4cff7
LeandroRodrigoMolina <LeandroRodrigoMolina@gmail.com>
2024-06-02 18:09:08 -0300

    [Fix] fix video and twin stream alert

--- botTwin.py
+++ botTwin.py
@@ -7,6 +7,7 @@ import logging
 test_channel = token_1.test_bot
 stream_channel = token_1.stream_twin
 
+main_channel = token_1.test_bot
 client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 # Configuración del logger
@@ -41,16 +42,16 @@ async def on_ready():
         logging.info("El bot está en línea y envió un mensaje de inicio.")
 
     # Iniciar tareas de verificación
-    #auto_check_live.start()  # Inicia la tarea para verificar si Twinsensei está en vivo
+    auto_check_live.start()  # Inicia la tarea para verificar si Twinsensei está en vivo
     check_latest_video.start()  # Inicia la tarea para verificar nuevos videos
 
     # Iniciar la tarea de cambio de estado cíclico
-    cycle_status.start()
+    #cycle_status.start()
 
-@tasks.loop(minutes=2)  # Verifica cada 1 minuto
+@tasks.loop(minutes=1)  # Verifica cada 1 minuto
 async def auto_check_live():
     try:
-        channel = client.get_channel(int(test_channel))  # stream twin
+        channel = client.get_channel(int(main_channel))  # stream twin
         live_url, title = await check_twinsensei_live()
         if live_url and title:
             await channel.send(f"@everyone ¡¡Twin sensei está transmitiendo en vivo!! **{title}**\n{live_url}")
@@ -72,14 +73,14 @@ async def cycle_status():
         await client.change_presence(status=discord.Status.online, activity=status)
         await asyncio.sleep(30)
 
-@tasks.loop(minutes=2)
+@tasks.loop(minutes=20)
 async def check_latest_video():
     try:
-        video_url = await find_latest_video(token_1.twin_channel_id)
+        video_url, title = await find_latest_video(token_1.twin_channel_id)
         if video_url:
-            general_channel = client.get_channel(int(test_channel))
+            general_channel = client.get_channel(int(main_channel))
             if general_channel:
-                await general_channel.send(f'@everyone ¡¡Twin subió un NUEVO VIDEO!! Míralo aquí: \n{video_url}')
+                await general_channel.send(f'@everyone ¡¡Twin subió un NUEVO VIDEO **{title}**!! Míralo aquí: \n{video_url}')
                 logging.info(f"Enviado mensaje de nuevo video: {video_url}")
             else:
                 print("No se encontró el canal para enviar el mensaje.")
--- funciones.py
+++ funciones.py
@@ -1,11 +1,11 @@
 import os
 from googleapiclient.discovery import build
-import isodate
 import aiohttp
 import json
 import token_1
 import asyncio
 from bs4 import BeautifulSoup
+from datetime import datetime
 
 session = None  # Global session for aiohttp
 
@@ -21,25 +21,27 @@ async def close_http_client():
 # Construcción del servicio de la API de YouTube
 youtube = build('youtube', 'v3', developerKey=token_1.api_key)
 
-# Funciones para manejar el último ID del stream publicado
-def load_last_live_id(file_path):
-    """Carga el último ID del stream publicado desde un archivo."""
+# Funciones para manejar los IDs de los streams en vivo
+def load_live_stream_ids(file_path):
+    """Carga todos los IDs de los streams en vivo desde un archivo."""
     try:
         with open(file_path, 'r') as file:
-            return file.read().strip()
+            return set(file.read().strip().split('\n'))
     except FileNotFoundError:
-        return None
+        return set()
 
-def save_last_live_id(video_id, file_path):
-    """Guarda el último ID del stream publicado en un archivo."""
-    with open(file_path, 'w') as file:
-        file.write(video_id)
+def save_live_stream_id(video_id, file_path):
+    """Agrega un ID de stream en vivo al archivo de registros."""
+    live_stream_ids = load_live_stream_ids(file_path)
+    if video_id not in live_stream_ids:  # Verificar si el ID no está ya en el archivo
+        with open(file_path, 'a') as file:
+            file.write(f"{video_id}\n")
 
-# Ruta del archivo para guardar el último ID del stream publicado
-last_live_file = './last_live_id.txt'
+# Ruta del archivo para guardar los IDs de los streams en vivo
+live_stream_file = './live_stream_ids.txt'
 
 async def check_twinsensei_live():
-    """Verifica si Twin Sensei está en vivo y devuelve el URL del live stream y el título."""
+    """Verifica si Twin Sensei está en vivo y guarda el ID del stream en vivo."""
     ytUserName = 'twinsensei'
     ytUrl = f"https://www.youtube.com/@{ytUserName}/live"
     async with session.get(ytUrl, cookies={'CONSENT': 'YES+42'}) as response:
@@ -53,11 +55,12 @@ async def check_twinsensei_live():
             title = liveJson["videoDetails"]['title']
             if live and status != "LIVE_STREAM_OFFLINE":
                 video_id = live['href'].split('=')[-1]  # Obtener el ID del video
+                live_stream_ids = load_live_stream_ids(live_stream_file)
+                if video_id in live_stream_ids:  # Verificar si el ID ya está guardado
+                    return None, None
+                save_live_stream_id(video_id, live_stream_file)
                 live_url = f"https://youtu.be/{video_id}"  # Usar el formato corto youtu.be
-                last_published_id = load_last_live_id(last_live_file)
-                if last_published_id != video_id:  # Verificar si el ID es diferente al último publicado
-                    save_last_live_id(video_id, last_live_file)
-                    return live_url, title
+                return live_url, title
     return None, None
 
 def getLiveJson(scripts):
@@ -83,6 +86,24 @@ def save_video_id(video_id, file_path):
     with open(file_path, 'a') as file:
         file.write(f"{video_id}\n")
 
+# Funciones para manejar la fecha mínima
+def load_min_date(file_path, default_date):
+    """Carga la fecha mínima desde un archivo, o usa una fecha por defecto."""
+    try:
+        with open(file_path, 'r') as file:
+            return datetime.strptime(file.read().strip(), "%Y-%m-%dT%H:%M:%SZ")
+    except FileNotFoundError:
+        return default_date
+
+def save_min_date(min_date, file_path):
+    """Guarda la fecha mínima en un archivo."""
+    with open(file_path, 'w') as file:
+        file.write(min_date.strftime("%Y-%m-%dT%H:%M:%SZ"))
+
+# Ruta del archivo para guardar la fecha mínima
+min_date_file = './min_date.txt'
+default_min_date = datetime.strptime("2024-05-27T07:50:23Z", "%Y-%m-%dT%H:%M:%SZ")
+
 # Archivo para guardar los IDs de los videos subidos
 video_ids_file = './video_ids.txt'
 
@@ -97,34 +118,19 @@ async def find_latest_video(channel_id):
     )
     response = request.execute()
     if len(response['items']) != 3:
-        print("Items del json: ",len(response['items']))
         return None, None  # Asegurarse de que haya exactamente 3 videos en la respuesta
-    
+
     recorded_ids = load_video_ids(video_ids_file)
+    live_stream_ids = load_live_stream_ids(live_stream_file)
+    min_date = load_min_date(min_date_file, default_min_date)
+
     for item in response['items']:
-        if item['snippet']['liveBroadcastContent'] == 'none':
-            video_id = item['id']['videoId']
-            if video_id in recorded_ids:  # Verificar si el ID no está en el registro
-                video_url = f"https://youtu.be/{video_id}"
-                print("video en el registro", video_url)
-                break
-            else:
-                save_video_id(video_id, video_ids_file)
-                video_url = f"https://youtu.be/{video_id}"
-                title = item['snippet']['title']
-                return video_url, title
+        video_id = item['id']['videoId']
+        published_at = datetime.strptime(item['snippet']['publishedAt'], "%Y-%m-%dT%H:%M:%SZ")
+        if published_at >= min_date and video_id not in recorded_ids and video_id not in live_stream_ids:
+            save_video_id(video_id, video_ids_file)
+            save_min_date(published_at, min_date_file)  # Actualizar la fecha mínima
+            video_url = f"https://youtu.be/{video_id}"
+            title = item['snippet']['title']
+            return video_url, title
     return None, None
-
-# Uso de las funciones
-async def main():
-    await init_http_client()
-    channel_id = token_1.twin_channel_id
-    video_url, title = await find_latest_video(channel_id)
-    if video_url and title:
-        print(f"Nuevo video subido: {title} - {video_url}")
-    else:
-        print("No hay nuevos videos subidos.")
-    await close_http_client()
-
-if __name__ == "__main__":
-    asyncio.run(main())
